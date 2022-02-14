3b5e907e777602d6156393aea8b8f90f02edde41
Lucas Alegre <lucasealegre123@gmail.com>
2022-02-14 19:52:49 -0300

    CAAMBIOS

--- src/GitHub/main.java
+++ src/GitHub/main.java
@@ -1,6 +1,7 @@
 package GitHub;
 
 
+
 class Empresa{
 	
 
@@ -64,6 +65,31 @@ class Trabajador{
 	
 	
 	
+
+class Region{
+	
+	private String pais;
+	private int codigoPostal;
+	
+	public Region (String pais, int codigoPostal) {
+		this.pais = pais;
+		this.codigoPostal = codigoPostal;	
+	}
+
+	public String getPais() {
+		return pais;
+	}
+	public int getcodigoPostal() {
+		return codigoPostal;
+	}
+	@Override
+	public String toString() {
+		return "Empresa ( " + getPais() + ", " +  getcodigoPostal()+ " )";
+	}
+}
+
+
+
 
 
 
